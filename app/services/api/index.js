2d051404e82022598fbe9a2cdef07a126b48d729
Giang Dao <giangdao@success-ss.com.vn>
2018-06-22 18:42:00 +0700

    use axios for api

---
+++ app/services/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { registerIntercepters } from './intercepters';
+import { API_URL } from '../../common/constants/url';
+
+const restApi = axios.create({
+  baseURL: API_URL,
+  headers: {
+    post: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+  },
+});
+
+registerIntercepters(restApi);
+
+export default restApi;
